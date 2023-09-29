4d2260cdcf77814d6ae1f2210f3af5e971820b22
Phuong Anh, Vo <chansoo194@gmail.com>
2023-09-29 14:07:25 +0700

    Update code-week1.py
    add time_from_utc and time_to_utc

--- code-week1.py
+++ code-week1.py
@@ -9,3 +9,31 @@ x = 4250
 print(get_hours(x))
 print(get_minutes(x))
 print(get_seconds(x))
+
+def to_24_hour_clock(hours):
+    return hours % 24
+    
+def time_to_utc(utc_offset, time):
+    hours = time - utc_offset
+    return to_24_hour_clock(hours)
+    
+def time_from_utc(utc_offset, time):
+    hours = time + utc_offset
+    return to_24_hour_clock(hours)
+
+
+print(time_to_utc(+0, 12.0))
+print(time_to_utc(+1, 12.0))
+print(time_to_utc(-1, 12.0))
+print(time_to_utc(-11, 18.0))
+print(time_to_utc(-1, 0.0))
+print(time_to_utc(-1, 23.0))
+
+print(time_from_utc(+0, 12.0))
+print(time_from_utc(+1, 12.0))
+print(time_from_utc(-1, 12.0))
+print(time_from_utc(+6, 6.0))
+print(time_from_utc(-7, 6.0))
+print(time_from_utc(-1, 0.0))
+print(time_from_utc(-1, 23.0))
+print(time_from_utc(+1, 23.0))
