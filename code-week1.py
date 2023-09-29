eb34313a5cd94457372031fafb33cf2070aee069
Phuong Anh, Vo <chansoo194@gmail.com>
2023-09-29 14:57:07 +0700

    Update code-week1.py
    Add functions seconds_difference, hours_difference, to_float_hours

--- code-week1.py
+++ code-week1.py
@@ -1,3 +1,13 @@
+def seconds_difference(time_1, time_2):
+    return time_2 - time_1
+
+def hours_difference(time_1, time_2):
+    return (time_2 - time_1) / 3600
+
+def to_float_hours(hours, minutes, seconds):
+    if 0 <= minutes < 60  and  0 <= seconds < 60:
+        return hours + (minutes / 60) + (seconds / 3600)
+
 def get_hours(number):
     return number // 3600
 def get_minutes(number):
@@ -5,10 +15,10 @@ def get_minutes(number):
 def get_seconds(number):
     return (number % 3600) % 60
 
-x = 4250
-print(get_hours(x))
-print(get_minutes(x))
-print(get_seconds(x))
+# x = 4250
+# print(get_hours(x))
+# print(get_minutes(x))
+# print(get_seconds(x))
 
 def to_24_hour_clock(hours):
     return hours % 24
@@ -22,18 +32,18 @@ def time_from_utc(utc_offset, time):
     return to_24_hour_clock(hours)
 
 
-print(time_to_utc(+0, 12.0))
-print(time_to_utc(+1, 12.0))
-print(time_to_utc(-1, 12.0))
-print(time_to_utc(-11, 18.0))
-print(time_to_utc(-1, 0.0))
-print(time_to_utc(-1, 23.0))
+# print(time_to_utc(+0, 12.0))
+# print(time_to_utc(+1, 12.0))
+# print(time_to_utc(-1, 12.0))
+# print(time_to_utc(-11, 18.0))
+# print(time_to_utc(-1, 0.0))
+# print(time_to_utc(-1, 23.0))
 
-print(time_from_utc(+0, 12.0))
-print(time_from_utc(+1, 12.0))
-print(time_from_utc(-1, 12.0))
-print(time_from_utc(+6, 6.0))
-print(time_from_utc(-7, 6.0))
-print(time_from_utc(-1, 0.0))
-print(time_from_utc(-1, 23.0))
-print(time_from_utc(+1, 23.0))
+# print(time_from_utc(+0, 12.0))
+# print(time_from_utc(+1, 12.0))
+# print(time_from_utc(-1, 12.0))
+# print(time_from_utc(+6, 6.0))
+# print(time_from_utc(-7, 6.0))
+# print(time_from_utc(-1, 0.0))
+# print(time_from_utc(-1, 23.0))
+# print(time_from_utc(+1, 23.0))
